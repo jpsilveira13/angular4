d476b330231bf25257a82ec3681889b4400367aa
João Paulo Silveira <samotinho@gmail.com>
2018-04-03 22:48:54 -0300

    Fim da aula 7

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService} from './cursos.service';
 
 @Component({
-  selector: 'app-cursos',
-  templateUrl: './cursos.component.html',
-  styleUrls: ['./cursos.component.css']
+    selector: 'app-cursos',
+    templateUrl: './cursos.component.html',
+    styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
 
-  nomePortal: string;
-  cursos: string[] = ['Java','Angular','PHP'];
+    nomePortal: string;
+    cursos: string[];
 
 
-  constructor() {
-    this.nomePortal = 'www.sortesim.com.br'
-  }
+    constructor(private  cursosService: CursosService) {
+        this.nomePortal = 'www.sortesim.com.br'
+        //var servicos = new CursosService();
+        this.cursos = this.cursosService.getCursos();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
