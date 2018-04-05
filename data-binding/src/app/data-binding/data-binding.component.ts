24d4a58b00ec4dd7f20d68b9f56dd9794f068833
João Paulo Silveira <samotinho@gmail.com>
2018-04-04 22:56:39 -0300

    Fim da aula 11

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -1,26 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-data-binding',
-  templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+    selector: 'app-data-binding',
+    templateUrl: './data-binding.component.html',
+    //styleUrls: ['./data-binding.component.css']
+    styles: [
+        `
+            .highlight{
+                background-color: yellow;
+                font-weight:bold;
+            }
+      `
+    ]
 })
 export class DataBindingComponent implements OnInit {
 
-  url: string = 'https://www.sortesim.com.br';
-  cursoAngular: boolean = true;
-  urlImagem: string = 'http://lorempixel.com/400/200/nature/';
-  getValor(){
-    return 1;
-  }
+    url: string = 'https://www.sortesim.com.br';
+    cursoAngular: boolean = true;
+    urlImagem: string = 'http://lorempixel.com/400/200/nature/';
+    valorAtual: string = '';
+    valorSalvo: string = '';
+    isMouseOver: boolean = false;
+    getValor(){
+        return 1;
+    }
 
-  getCurtirCurso(){
-    return true;
-  }
+    getCurtirCurso(){
+        return true;
+    }
+    botaoClicado(){
+        alert('cliquei');
+    }
+    onKeyUp(evento: KeyboardEvent){
+        this.valorAtual = ((<HTMLInputElement>evento.target).value);
+    }
+    onMouseOverOut(){
+        this.isMouseOver = !this.isMouseOver;
+    }
+    salvarValor(valor){
+        this.valorSalvo = valor;
+    }
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
